6491e945fce89631322e1012d1b876677a871821
Damian Mrowca <mrowca@gmx.de>
2016-12-19 18:02:37 -0800

    initial commit unity scenes

---
+++ unity-scenes/Assets/Scripts/MoveStick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveStick : MonoBehaviour {
+
+	public float min = 0.05f;
+	public float max = 0.05f;
+	// Use this for initialization
+	void Start () {
+		min=transform.position.x - 0.05f;
+        max=transform.position.x + 0.05f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position =new Vector3(Mathf.PingPong(Time.time/10,max-min)+min, transform.position.y, transform.position.z);
+	}
+}
---
+++ unity-scenes/Assets/Scripts/RevealBox.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RevealBox : MonoBehaviour {
+
+	private bool isRevealed;
+	// Use this for initialization
+	void Start () {
+		isRevealed = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+		{
+			if(isRevealed)
+			{
+		 		this.GetComponent<MeshRenderer>().enabled = false;
+				isRevealed = false;
+		 	}
+		 	else
+		 	{
+				this.GetComponent<MeshRenderer>().enabled = true;
+				isRevealed = true;
+		 	}
+		}
+	}
+}
