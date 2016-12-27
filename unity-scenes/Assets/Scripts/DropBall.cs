767bad0c2637c2b3c3d119914a2abc15cb717f78
Damian Mrowca <mrowca@gmx.de>
2016-12-27 20:38:29 +0100

    experiment SMBJ1992-E1 added

---
+++ unity-scenes/Assets/Scripts/DropBall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropBall : MonoBehaviour {
+
+	private Vector3 init_pos;
+	// Use this for initialization
+	void Start () {
+		init_pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("b")) 
+		{
+			Debug.Log("ball dropping");
+			GetComponent<Rigidbody>().useGravity = true;
+		}
+		if(Input.GetKeyDown("r"))
+		{
+			Debug.Log("ball reset");
+			GetComponent<Rigidbody>().useGravity = false;
+			transform.position = init_pos;
+		}
+	}
+}
