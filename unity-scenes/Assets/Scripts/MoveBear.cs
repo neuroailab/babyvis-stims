7478f3ff306bc848e1db59e4998063bd899b0b48
Damian Mrowca <mrowca@gmx.de>
2016-12-27 18:57:14 +0100

    experiment W1992 added

---
+++ unity-scenes/Assets/Scripts/MoveBear.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBear : MonoBehaviour {
+
+	public float speed = 1.0F;
+	private Vector3 down;
+	private Vector3 up;
+	private Vector3 left;
+	private Vector3 right;
+	private float startTime;
+	private float distance;
+
+	private bool isUp = false;
+	private bool isDown = false;
+	private bool isLeft = false;
+	private bool isRight = false;
+
+	// Use this for initialization
+	void Start () {
+		down = transform.position;
+		up = transform.position;
+		right = transform.position;
+		left = transform.position;
+		down.y = down.y - 1f;
+		left.x = left.x - 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("left")) 
+		{
+			Debug.Log("bear left");
+			startTime = Time.time;
+			distance = Vector3.Distance(right, left);
+			isLeft = true;
+			isDown = false;
+			isUp = false;
+			isRight = false;
+		}
+		if(Input.GetKeyDown("right")) 
+		{
+			Debug.Log("bear right");
+			startTime = Time.time;
+			distance = Vector3.Distance(left, right);
+			isLeft = false;
+			isDown = false;
+			isUp = false;
+			isRight = true;
+		}
+		if(Input.GetKeyDown("down")) 
+		{
+			Debug.Log("bear down");
+			startTime = Time.time;
+			distance = Vector3.Distance(up, down);
+			isLeft = false;
+			isDown = true;
+			isUp = false;
+			isRight = false;
+		}
+		if(Input.GetKeyDown("up")) 
+		{
+			Debug.Log("bear up");
+			startTime = Time.time;
+			distance = Vector3.Distance(down, up);
+			isLeft = false;
+			isDown = false;
+			isUp = true;
+			isRight = false;
+		}
+
+		if(distance != 0)
+		{
+			float fracDistance = ((Time.time - startTime) * speed) / distance;
+			if(isLeft) {
+				left.y = transform.position.y;
+				right.y = left.y;
+				transform.position = Vector3.Lerp(right, left, fracDistance);
+			}	
+			if(isRight) {
+				left.y = transform.position.y;
+				right.y = left.y;
+				transform.position = Vector3.Lerp(left, right, fracDistance);
+			}
+			if(isDown) {
+				up.x = transform.position.x;
+				down.x = up.x;
+				transform.position = Vector3.Lerp(up, down, fracDistance);
+			}
+			if(isUp) {
+				up.x = transform.position.x;
+				down.x = up.x;
+				transform.position = Vector3.Lerp(down, up, fracDistance);
+			}
+		}
+	}
+}
---
+++ unity-scenes/Assets/Scripts/MoveScreen.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScreen : MonoBehaviour {
+
+	public float speed = 1.0F;
+	private Vector3 down;
+	private Vector3 up;
+	private float startTime;
+	private float distance;
+
+	private bool isUp = true;
+
+	// Use this for initialization
+	void Start () {
+		down = transform.position;
+		up = transform.position;
+		up.y = up.y + 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire2")) 
+		{
+			if(isUp)
+			{
+				Debug.Log("screen up");
+				startTime = Time.time;
+				distance = Vector3.Distance(up, down);
+				isUp = false;
+			}
+			else
+			{
+				Debug.Log("screen down");
+				startTime = Time.time;
+				distance = Vector3.Distance(down, up);
+				isUp = true;
+			}
+		}
+
+		if(distance != 0)
+		{
+			float fracDistance = ((Time.time - startTime) * speed) / distance;
+			if(isUp)
+				transform.position = Vector3.Lerp(up, down, fracDistance);	
+			else
+				transform.position = Vector3.Lerp(down, up, fracDistance);
+		}
+	}
+}
